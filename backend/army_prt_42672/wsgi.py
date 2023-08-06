a6e6fc2721fa7300503384f534b78281e11baa78
Crowdbotics <team@crowdbotics.com>
2023-08-06 01:35:45 +0000

    Initial commit.

---
+++ backend/army_prt_42672/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for army_prt_42672 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'army_prt_42672.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "army-prt-42672",
+  url: "https://army-prt-42672.botics.co",
+  api: "https://army-prt-42672.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
